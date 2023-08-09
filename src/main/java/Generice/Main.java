cd296120b58ba3515ba040a36050188eccf0d13f
张书猛 <916406821@qq.com>
2023-08-09 11:08:45 +0800

    泛型:修改包名

--- src/main/java/Generice/Main.java
+++ src/main/java/Generice/Main.java
@@ -1,4 +1,4 @@
-package Generics;
+package Generice;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/main/java/Generice/UseGenerice/Main.java
+++ src/main/java/Generice/UseGenerice/Main.java
@@ -1,4 +1,4 @@
-package Generics.UseGenerice;
+package Generice.UseGenerice;
 
 import java.util.ArrayList;
 import java.util.Arrays;
