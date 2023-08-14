b75f78607f1991063341ac74f689660b90ebd185
张书猛 <916406821@qq.com>
2023-08-14 18:02:23 +0800

    集合:使用Map

---
+++ src/main/java/Collection/UseMap/Test.java
@@ -0,0 +1,58 @@
+package Collection.UseMap;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+// 练习
+// 请编写一个根据name查找score的程序，并利用Map充当缓存，以提高查找效率：
+public class Test {
+    public static void main(String[] args) {
+        List<Student> list = List.of(
+                new Student("Bob", 78),
+                new Student("Alice", 85),
+                new Student("Brush", 66),
+                new Student("Newton", 99));
+        var holder = new Students(list);
+        System.out.println(holder.getScore("Bob") == 78 ? "测试成功!" : "测试失败!");
+        System.out.println(holder.getScore("Alice") == 85 ? "测试成功!" : "测试失败!");
+        System.out.println(holder.getScore("Tom") == -1 ? "测试成功!" : "测试失败!");
+    }
+}
+
+class Students {
+    List<Student> list;
+    Map<String, Integer> cache;
+
+    Students(List<Student> list) {
+        this.list = list;
+        cache = new HashMap<>();
+    }
+
+    /**
+     * 根据name查找score，找到返回score，未找到返回-1
+     */
+    int getScore(String name) {
+        // 先在Map中查找:
+        Integer score = this.cache.get(name);
+        if (score == null) {
+            // TODO:
+            // 如果缓存中不存在，从原始列表中查找:
+            score = findInList(name);
+            // 将找到的值加入缓存:
+            if (score != null) {
+                this.cache.put(name, score);
+            }
+        }
+        return score == null ? -1 : score.intValue();
+    }
+
+    Integer findInList(String name) {
+        for (var ss : this.list) {
+            if (ss.name.equals(name)) {
+                return ss.score;
+            }
+        }
+        return null;
+    }
+}
